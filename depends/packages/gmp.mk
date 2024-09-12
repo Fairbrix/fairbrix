80ce530483ccdea685ec8245ee4e9ee72acb6de3
Ahmed Bodiwala <ahmed_bodi@msn.com>
2024-09-12 22:48:48 +0100

    build: WIP Automatic Builds

--- depends/packages/gmp.mk
+++ depends/packages/gmp.mk
@@ -1,19 +1,14 @@
 package=gmp
 $(package)_version=6.2.1
-$(package)_sha256_hash=fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2
-$(package)_file_name=$(package)-$($(package)_version).tar.xz
-$(package)_download_path=https://gmplib.org/download/$(package)
+$(package)_download_path=https://ftp.gnu.org/gnu/gmp/
+$(package)_file_name=$(package)-$($(package)_version).tar.bz2
+$(package)_sha256_hash=eae9326beb4158c386e39a356818031bd28f3124cf915f8c5b1dc4c7a36b4d7c
 
 define $(package)_set_vars
-  $(package)_config_opts=--prefix=$(host_prefix)
-  $(package)_config_opts += --disable-shared --enable-static --enable-cxx
-  $(package)_config_opts += --enable-option-checking
-  $(package)_config_opts_linux=--with-pic
-  $(package)_config_opts_darwin += --with-pic
-endef
-
-define $(package)_preprocess_cmds
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub .
+$(package)_config_opts=--disable-shared
+$(package)_config_opts_mingw32=--enable-mingw
+$(package)_config_opts_linux=--with-pic
+$(package)_config_opts_darwin=--with-pic
 endef
 
 define $(package)_config_cmds
@@ -26,8 +21,4 @@ endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
-endef
-
-define $(package)_postprocess_cmds
-  sed -i "s|libstdc++.la|libstdc++.a|" lib/libgmpxx.la
 endef
